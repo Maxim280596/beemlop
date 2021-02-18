5ac2ae53d37db364c0acb5b3ce87edf889d6f49a
maxim <max280596@gmail.com>
2021-02-18 14:14:33 +0200

    dddd

--- js/opt.js
+++ js/opt.js
@@ -9,7 +9,7 @@ const options = {
 
 function handleImg (myImg, observer) {
   myImg.forEach(myImgSingle => {
-    console.log(myImgSingle.intersectionRatio);
+
     if (myImgSingle.intersectionRatio > 0) {
       loadImage(myImgSingle.target);
     }
